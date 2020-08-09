17e7d59641c13d19a830c3ade8517fe170b2d196
Mateo Llano <mateo.llano1@gmail.com>
2020-08-09 16:18:20 -0500

    product style fixxed

--- src/app/config/url.ts
+++ src/app/config/url.ts
@@ -1,2 +1,2 @@
-// export const url='http://www.edenhub.co:5200/api'
-export const url='http://localhost:3000/api'
+export const url='https://api.edenhub.co/api/'
+// export const url='http://localhost:3000/api'
--- src/app/pipes/price.pipe.ts
+++ src/app/pipes/price.pipe.ts
@@ -6,34 +6,37 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PricePipe implements PipeTransform {
 
   transform(price: string) :string {
-    let newPrice: string = "";
-    let cadenaNueva="" ;
-    let cont=0
-    for (var i = price.length - 1; i >= 0; i--) {   
-      if(cont==4){
-        newPrice+= price.charAt(i);
-        newPrice+= price.charAt(Number(','))
-      }else{
-        newPrice+= price.charAt(i);
-      }
-    }
-    cont+=1
+    // let newPrice: string = "";
+    // let cadenaNueva="" ;
+    // let cont=0
+    let init = price.slice(0,price.length-3)
+    let end = price.slice(price.length-3,price.length)
+    return `$${init},${end} COP`
+    // for (var i = price.length - 1; i >= 0; i--) {   
+    //   if(cont==4){
+    //     newPrice+= price.charAt(i);
+    //     newPrice+= price.charAt(Number(','))
+    //   }else{
+    //     newPrice+= price.charAt(i);
+    //   }
+    // }
+    // cont+=1
 
-    cadenaNueva=this.invertir(newPrice)
-    cadenaNueva ="$"+cadenaNueva+" "+"COP"
-    console.log(cadenaNueva)
-    return cadenaNueva;
+    // cadenaNueva=this.invertir(newPrice)
+    // cadenaNueva ="$"+cadenaNueva+" "+"COP"
+    // console.log(cadenaNueva)
+    // return cadenaNueva;
   }
   
  
-  invertir(cadena) {
-    var x = cadena.length;
-    var cadenaInvertida = "";
-    while (x>=0) {
-      cadenaInvertida = cadenaInvertida + cadena.charAt(x);
-      x--;
-    }
-    return cadenaInvertida;
-  }
+  // invertir(cadena) {
+  //   var x = cadena.length;
+  //   var cadenaInvertida = "";
+  //   while (x>=0) {
+  //     cadenaInvertida = cadenaInvertida + cadena.charAt(x);
+  //     x--;
+  //   }
+  //   return cadenaInvertida;
+  // }
 
 }
