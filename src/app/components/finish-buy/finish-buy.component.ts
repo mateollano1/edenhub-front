29e71b1c123d5c844f6f2680c841927737c175d4
Mateo Llano <mateo.llano1@gmail.com>
2020-08-10 22:01:04 -0500

    friendlyid added

--- src/app/components/finish-buy/finish-buy.component.ts
+++ src/app/components/finish-buy/finish-buy.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../../services/products.service';
 import { FormGroup, FormControl, Validators, Form } from '@angular/forms';
 import Swal from 'sweetalert2'
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-finish-buy',
   templateUrl: './finish-buy.component.html',
@@ -11,7 +12,8 @@ export class FinishBuyComponent implements OnInit {
   products: any[]
   finalPrice: ""
   public bill: FormGroup
-  constructor(private productsService: ProductsService) {
+  constructor(private productsService: ProductsService,
+    private router: Router) {
     window.scroll(0, 0);
   }
 
@@ -53,58 +55,64 @@ export class FinishBuyComponent implements OnInit {
   }
 
   savePurchase() {
-    if (this.products.length > 0) {
-      if (this.bill.valid) {
-        let newFormatProduct = []
-        for (let index = 0; index < this.products.length; index++) {
-          const element = this.products[index];
-          let prod = {
-            'ammount': element['ammount'],
-            'idProduct': element['product']['_id'],
-          }
-          newFormatProduct.push(prod)
-        }
-        let purchase = {
-          'name': this.bill.value['name'],
-          'location': this.bill.value['location'],
-          'telephone': this.bill.value['telephone'],
-          'note': this.bill.value['note'],
-          'totalPrice': this.finalPrice,
-          'products': newFormatProduct
-        }
-        this.productsService.postPurchase(purchase).subscribe(data => {
-          sessionStorage.clear()
-          Swal.fire({
-            title: 'Compra finalizada exitosamente',
-            text: '¡Gracias por tu compra!',
-            icon: 'success',
-            confirmButtonText: 'Ok',
-            confirmButtonColor: '#008000'
-
-          })
-
-        })
-
-      } else {
-        // console.log("invalid");
-        Swal.fire({
-          title: 'Formulario invalido',
-          text: 'Por favor ingrese correctamente los datos',
-          icon: 'error',
-          confirmButtonText: 'Ok',
-          confirmButtonColor: '#008000',
-        })
-      }
-    }
-    else {
+    if (this.products == undefined) {
       Swal.fire({
-        title: 'Error!',
-        text: 'Do you want to continue',
+        title: 'Acción inválida',
+        text: 'Por favor selecciona primero el producto que deseas comprar.',
+        icon: 'error',
+        confirmButtonText: 'Ok',
+        confirmButtonColor: '#008000',
+      })
+      return
+    }
+    if (this.bill.valid) {
+      let newFormatProduct = []
+      for (let index = 0; index < this.products.length; index++) {
+        const element = this.products[index];
+        let prod = {
+          'ammount': element['ammount'],
+          'idProduct': element['product']['_id'],
+        }
+        newFormatProduct.push(prod)
+      }
+      let purchase = {
+        'name': this.bill.value['name'],
+        'location': this.bill.value['location'],
+        'telephone': this.bill.value['telephone'],
+        'note': this.bill.value['note'],
+        'totalPrice': this.finalPrice,
+        'products': newFormatProduct
+      }
+
+      this.productsService.postPurchase(purchase).subscribe(data => {
+        sessionStorage.clear()
+        let a= "2323"
+        Swal.fire({
+          title: '¡Gracias por tu compra!',
+          html:
+            '<span id="swal2-content" style="font-size:15px;">En estos momentos nos disponemos a preparar tu pedido para llevarlo hasta la puerta de tu casa. <br></span>' +
+            '<span style="font-size:12px; color: #008000;">**</span>' +
+            '<span style="font-size:12px; text-align: left;"> Si tienes alguna duda de tu pedido no dudes en escribirnos a nuestro Whatsapp, Tú número de orden es: </span>' +
+
+            `<input style="text-align:center" value=${data['newPurchase']['friendlyId']} class="swal2-input" disabled>`,
+          icon: 'success',
+          confirmButtonText: 'Ok',
+          confirmButtonColor: '#008000'
+        }).then((result) => {
+          this.router.navigateByUrl('#')
+        })
+
+      })
+
+    } else {
+      // console.log("invalid");
+      Swal.fire({
+        title: 'Formulario inválido',
+        text: 'Por favor ingrese correctamente los datos',
         icon: 'error',
         confirmButtonText: 'Ok',
         confirmButtonColor: '#008000',
       })
     }
   }
-
 }
